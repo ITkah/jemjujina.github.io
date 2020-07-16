8e4911cf7ae97645a4390684c6257e4287037608
ITkah <ilatkacenko132@gmail.com>
2020-07-16 16:51:15 +0300

    first-download

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+            titleSrc: function(item) {
+                return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+            }
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+    });
+    $('.phone').on('input', function() {
+        $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''));
+    });
+
+    $('.name').on('input', function() {
+        $(this).val($(this).val().replace(/[0-9,+-_/';:}{}?.<>,()*&^%$#@!]/, ''));
+    });
+});
