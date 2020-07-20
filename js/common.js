4db24892018c8c92af3f2913108ad8b766bd550b
ITkah <ilatkacenko132@gmail.com>
2020-07-20 18:54:11 +0300

    preloader

--- js/common.js
+++ js/common.js
@@ -104,17 +104,16 @@ $(document).ready(function() {
         ]
     });
 
-//     var bLazy = new Blazy({
-//         breakpoints: [{
-//         width: 420,
-//         src: 'data-src-small'
-// 	}]
-//       , success: function(element){
-// 	    setTimeout(function(){
-// 		var parent = element.parentNode;
-// 		parent.className = parent.className.replace(/\bloading\b/,'');
-// 	    }, 200);
-//         }
-//    });
+    var bLazy = new Blazy({
+        breakpoints: [{
+       
+	}]
+      , success: function(element){
+	    setTimeout(function(){
+		var parent = element.parentNode;
+		parent.className = parent.className.replace(/\bloading\b/,'');
+	    }, 100);
+        }
+   });
 
 });
